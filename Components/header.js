a9e2c9621bd381cdde70c0726b17ccc572524ca6
venom <abdo30201080101694@gmail.com>
2024-08-18 17:48:15 +0300

    extract header to component

---
+++ Components/header.js
@@ -0,0 +1,18 @@
+export let headerContent = `
+    <div class="user-header-branding">
+        <img class="user-logo-image" src="https://cdn-icons-png.flaticon.com/512/2591/2591253.png" alt="logo">
+        <p class="user-logo-text"></p>E-commerce</p>
+    </div>
+    <div class="user-header-links">
+        <ul class="user-header-links-list">
+            <li class="user-header-links-item home"> Home </li>
+            <li class="user-header-links-item categories"> Categories </li>
+            <li class="user-header-links-item about"> About </li>
+            <li class="user-header-links-item contact"> Contact </li>
+        </ul>
+    </div>
+    <div class="user-header-logout">
+        <button class="user-header-logout-button">Logout</button>
+    </div>
+`;
+
--- index.js
+++ index.js
@@ -1,22 +1,24 @@
+import { headerContent } from "./Components/header.js";
+
 let content = document.getElementById("root");
 
-let headerContent = `
-    <div class="user-header-branding">
-        <img class="user-logo-image" src="https://cdn-icons-png.flaticon.com/512/2591/2591253.png" alt="logo">
-        <p class="user-logo-text"></p>E-commerce</p>
-    </div>
-    <div class="user-header-links">
-        <ul class="user-header-links-list">
-            <li class="user-header-links-item home"> Home </li>
-            <li class="user-header-links-item categories"> Categories </li>
-            <li class="user-header-links-item about"> About </li>
-            <li class="user-header-links-item contact"> Contact </li>
-        </ul>
-    </div>
-    <div class="user-header-logout">
-        <button class="user-header-logout-button">Logout</button>
-    </div>
-`;
+// let headerContent = `
+//     <div class="user-header-branding">
+//         <img class="user-logo-image" src="https://cdn-icons-png.flaticon.com/512/2591/2591253.png" alt="logo">
+//         <p class="user-logo-text"></p>E-commerce</p>
+//     </div>
+//     <div class="user-header-links">
+//         <ul class="user-header-links-list">
+//             <li class="user-header-links-item home"> Home </li>
+//             <li class="user-header-links-item categories"> Categories </li>
+//             <li class="user-header-links-item about"> About </li>
+//             <li class="user-header-links-item contact"> Contact </li>
+//         </ul>
+//     </div>
+//     <div class="user-header-logout">
+//         <button class="user-header-logout-button">Logout</button>
+//     </div>
+// `;
 
 let header = document.createElement("div");
 header.className = "user-header";
